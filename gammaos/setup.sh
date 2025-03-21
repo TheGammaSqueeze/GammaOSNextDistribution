afd6974d0f2e99dedd20d1910cd07bfa1889dfae
gamma <thegammasqueeze@gmail.com>
2025-03-21 22:00:00 +0000

    Fix dark theme and remove private DNS settings defaults

--- gammaos/setup.sh
+++ gammaos/setup.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3630a9ae10c4e7473030b310d25077050579c2fbce585826d68ddd81a3263d4c
-size 8074
+oid sha256:6a2afe0b16db21aeb0b0c08e72f11ae9d700074e7f708c233149008f0ea6acc0
+size 8076
