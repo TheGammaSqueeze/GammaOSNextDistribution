865a17447c3b1cf0e5001f439292e90943015fd9
gamma <thegammasqueeze@gmail.com>
2025-07-07 00:07:37 +0100

    Disable Google Play Protect by default, bypassing RA install issue on GMS builds

--- gammaos/customization.sh
+++ gammaos/customization.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:59892ba3f9f74e49882b8adca65e59fb1706d5cd308226bf01af23c58111cab7
-size 4604
+oid sha256:5cb5f93c8bd20a1bc1f19afd88354e79db3758c33f5d2aa83b9480fa3ba99727
+size 4661
--- gammaos/setup.sh
+++ gammaos/setup.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:183ecbad260b82c3a90287a03ef20ec1895107a7b7cf0d80504635846a13d150
-size 7992
+oid sha256:14d70547155d974e3db977ed2a160c03a7ec321d9cb069ceab554f5459bf9a7d
+size 8052
